f7036bc1a7942ae5e9cfb9589fef5575de10150c
sajjad <sajjad.darvish.yektayi@gmail.com>
2024-02-04 21:37:38 +0330

    Create mobile sidebar

--- app/components/sidebar/sidebar.tsx
+++ app/components/sidebar/sidebar.tsx
@@ -1,9 +1,11 @@
 import { DesktopSidebar } from "./desktop-sidebar";
+import MobileFooter from "./mobile-footer";
 
 export async function Sidebar({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-full">
       <DesktopSidebar />
+      <MobileFooter />
       <main className="lg:pl-20 h-full">{children}</main>
     </div>
   );
